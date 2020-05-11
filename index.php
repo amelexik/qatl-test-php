35208acc3fd7facf260e923a060728a421ef5ce3
amelexik <amelexik@gmail.com>
2020-05-11 07:52:20 +0300

    add .htaccess, fix entry point

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+// точка входа
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require_once '../qatl/Qatl.php';
+require_once 'qatl/Qatl.php';
 
 $config = CONFIG_PATH . DS . 'web.php';
 if (!file_exists($config))
